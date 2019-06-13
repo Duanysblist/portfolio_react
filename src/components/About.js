17d53c0b86dd4e6ec30d3db68ab1cda004159904
Daniel Duany <dduany919@gmail.com>
2019-06-12 21:02:40 -0400

    Added information to about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 class About extends React.Component {
   render() {
-    return <h1>About</h1>;
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          Hey there! I'm Dan, aspiring software developer and ex-cook. I spend
+          most of my days trying to absorb as much information as I can. I've
+          worked on building full stack web applications in Java using
+          Bootstrap, JQuery, Maven, Spring, and MySQL.I've also been gaining
+          more experience using React and Redux, even creating a full stack web
+          application using the MERN stack. I've gone to an exhaustingly long
+          list of meetups and have even done the Grow With Google CUNY Hackathon
+          in an attempt to make a voice controlled device to help students with
+          diasbilities in the classroom.{' '}
+        </p>
+        <br />
+        <p>
+          I'm really looking for a forward-thinking company to work for that I
+          can pour myself into. I've played around with Android application
+          development as well as Angular. My attention lies in mobile
+          application development, but I can definitely be swayed by the right
+          company and culture.
+        </p>
+      </div>
+    );
   }
 }
 
